f6e1e8729a9a5e8da3361a23da6a90d381aaaf17
Derek Paul <dereksamuelgr@gmail.com>
2021-03-31 16:38:35 -0500

    watch removed from webpack prod

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,6 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
-  watch: true,
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
